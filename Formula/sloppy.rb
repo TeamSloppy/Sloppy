dc3ee37f33955d8fdd72c661275d9a52e675ddb6
Vladislav Prusakov <spectraldragon@bk.ru>
2026-04-17 11:57:47 +0300

    Update sloppy.rb

--- Formula/sloppy.rb
+++ Formula/sloppy.rb
@@ -1,9 +1,9 @@
 class Sloppy < Formula
   desc "Agent runtime and dashboard for Sloppy"
   homepage "https://github.com/TeamSloppy/Sloppy"
-  version "1.0.0"
-  url "https://github.com/TeamSloppy/Sloppy/releases/download/v1.0.0/Sloppy-linux-x86_64.tar.gz"
-  sha256 "273c3247aa3a7a308a2daa22ac145c6f67abc4f79ed2bb17ecb85f742ad7e6be"
+  version "1.1.2"
+  url "https://github.com/TeamSloppy/Sloppy/releases/download/v1.1.2/Sloppy-linux-x86_64.tar.gz"
+  sha256 "89c44dc7e3f9954c5be3df64bb4b1e83636bcb35d8f02cde132b72ec7708fd64"
   license "MIT"
 
   def install
